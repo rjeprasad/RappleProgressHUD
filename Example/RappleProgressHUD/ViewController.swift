8b87c42b570b525bb840de3565b4148e3357e1ff
Rajeev Prasad <rajeev@Rajeevs-MacBook-Pro-2.local>
2015-11-15 19:50:01 +0800

    initial commit

--- Example/RappleProgressHUD/ViewController.swift
+++ Example/RappleProgressHUD/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func startProgress(sender : AnyObject?) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
