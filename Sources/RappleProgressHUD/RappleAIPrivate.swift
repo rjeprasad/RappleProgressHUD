fd8156d6f82c03c33c90ce641e89c8dc8a370b67
tayseer5 <tayseer.anwar92@gmail.com>
2022-05-09 09:48:33 +0200

    fix issue of for keyWindow in iOS 13
    more handling for UiWindow
    handle if ios is 13 or higher and handling if cannot get uiwindow object try to get if from other ways

--- Sources/RappleProgressHUD/RappleAIPrivate.swift
+++ Sources/RappleProgressHUD/RappleAIPrivate.swift
@@ -105,8 +105,22 @@ extension RappleActivityIndicatorView {
     }
     
     /** get key window */
-    @objc var keyWindow: UIWindow {
-        return UIApplication.shared.keyWindow!
+   @objc var keyWindow: UIWindow {
+    if #available(iOS 13.0, *) {
+        return UIApplication.shared.connectedScenes
+        // Keep only active scenes, onscreen and visible to the user
+            .filter { $0.activationState == .foregroundActive }
+        // Keep only the first `UIWindowScene`
+            .first(where: { $0 is UIWindowScene })
+        // Get its associated windows
+            .flatMap({ $0 as? UIWindowScene })?.windows
+        // Finally, keep only the key window
+            .first(where: \.isKeyWindow) ?? UIApplication.shared.windows.first(where: { $0.isKeyWindow })!
+    } else {
+    // Fallback on earlier versions
+        return (UIApplication.shared.windows.first(where: { $0.isKeyWindow })) ?? (UIApplication.shared.delegate?.window ??  UIApplication.shared.keyWindow)!
+    }
+        
     }
 }
 
